91fe0a20262cac85ad2dbd9767154422f3564ced
Venson <venson@teahouse.finance>
2024-03-01 16:17:56 +0800

    modify UniswapV3PathRecommender deploy script

--- scripts/deployUniswapV3PathRecommender.js
+++ scripts/deployUniswapV3PathRecommender.js
@@ -6,16 +6,8 @@ const config = JSON.parse(rawConfig)["UniswapV3PathRecommender"];
 
 async function main() {
     const UniswapV3PathRecommender = await ethers.getContractFactory("UniswapV3PathRecommender");
-    // const uniswapV3PathRecommender = await UniswapV3PathRecommender.deploy();
-    // console.log("UniswapV3PathRecommender depolyed", uniswapV3PathRecommender.target);
-
-    for (let i = 0; i < config.length; i++) {
-        // await uniswapV3PathRecommender.setRecommendedPath(config[i]["tokens"], config[i]["fees"]);
-        console.log('-------------------------------------');
-        console.log(`Path ${i + 1} set!`);
-        console.log(`\tTokens: ${config[i]["tokens"]}`);
-        console.log(`\tFees: ${config[i]["fees"]}`);
-    }
+    const uniswapV3PathRecommender = await UniswapV3PathRecommender.deploy();
+    console.log("UniswapV3PathRecommender depolyed", uniswapV3PathRecommender.target);
 }
 
 main().catch((error) => {
