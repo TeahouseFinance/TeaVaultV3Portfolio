756f66a5003671e122529167065df102b40abb4b
Venson <venson@teahouse.finance>
2024-03-01 16:37:48 +0800

    add path setter

---
+++ scripts/setUniswapV3PathRecommender.js
@@ -0,0 +1,25 @@
+const { ethers, upgrades } = require("hardhat");
+const fs = require('fs');
+
+const rawConfig = fs.readFileSync("./scripts/config.json");
+const config = JSON.parse(rawConfig)["UniswapV3PathRecommender"];
+
+async function main() {
+    const UniswapV3PathRecommender = await ethers.getContractFactory("UniswapV3PathRecommender");
+    const uniswapV3PathRecommender = UniswapV3PathRecommender.attach("0xF641059c53e272971199E01A748ca149bc86C46C");
+    // const uniswapV3PathRecommender = await UniswapV3PathRecommender.deploy();
+    // console.log("UniswapV3PathRecommender depolyed", uniswapV3PathRecommender.target);
+
+    for (let i = 0; i < config.length; i++) {
+        await uniswapV3PathRecommender.setRecommendedPath(config[i]["tokens"], config[i]["fees"]);
+        console.log('-------------------------------------');
+        console.log(`Path ${i + 1} set!`);
+        console.log(`\tTokens: ${config[i]["tokens"]}`);
+        console.log(`\tFees: ${config[i]["fees"]}`);
+    }
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
